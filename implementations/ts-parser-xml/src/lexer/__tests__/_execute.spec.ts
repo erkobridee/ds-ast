9978c4dbc01addd306b8480af0e3f4f6f83b3eda
erkobridee <erko.bridee@gmail.com>
2024-07-08 17:02:33 +0200

    update(ts-parser-xml): Lexer tests general execution

--- implementations/ts-parser-xml/src/lexer/__tests__/_execute.spec.ts
+++ implementations/ts-parser-xml/src/lexer/__tests__/_execute.spec.ts
@@ -1,6 +1,29 @@
+const executeIt = (name: string, specFn: Function) => {
+  it(name, () => {
+    console.log('');
+    specFn();
+    console.log(`\n${'-'.repeat(80)}`);
+  });
+};
+
+const executeRexExp = (
+  name: string,
+  { REGEXP, str }: { REGEXP: RegExp; str: string }
+) => {
+  executeIt(name, () => {
+    console.log(REGEXP.exec(str));
+  });
+};
+
 describe('Lexer', () => {
-  it('execute', () => {
-    // TODO: define the execution code
-    console.log('Lexer > execute');
+  executeRexExp('tag', {
+    REGEXP: /<(?<tag>[\w:]+)[\s\S]*?>(?<content>[\s\S]*)?<\/\k<tag>*?>/,
+    str: '<greetings>Hello World</greetings>',
+  });
+
+  executeRexExp('tag', {
+    REGEXP:
+      /<(?<tag>[\w:]+)(?:(?<attributes>(\s(\w+)=(?:"(.+)"|'(.+)'))+)|[\s\S]*?)>(?<content>[\s\S]*)?<\/\k<tag>*?>/,
+    str: '<greetings attr1="1" attr2=\'2\'>Hello World</greetings>',
   });
 });
